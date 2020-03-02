d5497ff1fea211a790dc4f868907188f6169d497
CrunchyMusli <HPham1@gmx.de>
2020-03-02 12:33:41 +0100

    added material.io

--- app/src/main/java/com/example/plantguide/MainActivity.java
+++ app/src/main/java/com/example/plantguide/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.plantguide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String Tag = "loggerMusli";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        RelativeLayout crunchyL = new RelativeLayout(this);
+        Button but = new Button(this);
+        crunchyL.addView(but);
+        crunchyL.setBackgroundColor(Color.GRAY);
+        setContentView(crunchyL);
+        but.setBackgroundColor(Color.RED);
+
+        //setContentView(R.layout.activity_main);
+        Log.i(Tag, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Tag, "onStart");
+
     }
 }
